baa634fc1037b498dfd476a31fbab77ad2108938
Manasseh <believemanasseh@gmail.com>
2023-03-02 09:33:57 +0100

    add event handlers for webs component

--- components/Webs/Webs.jsx
+++ components/Webs/Webs.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 
 import black from '@/public/black.png';
 import bookmark from '@/public/bookmark.png';
+import bookmarkWhite from '@/public/bookmarkwhite.png';
 import comment from '@/public/comment.png';
 import favorite from '@/public/favorite.png';
+import favoriteWhite from '@/public/favoritewhite.png';
 import retwitch from '@/public/retwitch.png';
 
 export default function Webs(props) {
+	const [liked, setLiked] = useState(false);
+	const [bookmarked, setBookmarked] = useState(false);
+
+	function handleLikeClick() {
+		setLiked(!liked);
+	}
+
+	function handleBookmarkClick() {
+		setBookmarked(!bookmarked);
+	}
+
 	return (
 		<>
 			<div className={styles.feed}>
@@ -43,20 +57,44 @@ export default function Webs(props) {
 							height={20}
 							width={20}
 						/>
-						<Image
-							className={styles.actionBtn}
-							src={favorite}
-							alt='like post'
-							height={20}
-							width={20}
-						/>
-						<Image
-							className={styles.actionBtn}
-							src={bookmark}
-							alt='bookmark post'
-							height={20}
-							width={20}
-						/>
+						{liked ? (
+							<Image
+								className={styles.actionBtn}
+								src={favorite}
+								alt='like post'
+								height={20}
+								width={20}
+								onClick={handleLikeClick}
+							/>
+						) : (
+							<Image
+								className={styles.actionBtn}
+								src={favoriteWhite}
+								alt='like post white'
+								height={20}
+								width={20}
+								onClick={handleLikeClick}
+							/>
+						)}
+						{bookmarked ? (
+							<Image
+								className={styles.actionBtn}
+								src={bookmark}
+								alt='bookmark post'
+								height={20}
+								width={20}
+								onClick={handleBookmarkClick}
+							/>
+						) : (
+							<Image
+								className={styles.actionBtn}
+								src={bookmarkWhite}
+								alt='bookmark post white'
+								height={20}
+								width={20}
+								onClick={handleBookmarkClick}
+							/>
+						)}
 					</span>
 				</div>
 			</div>
