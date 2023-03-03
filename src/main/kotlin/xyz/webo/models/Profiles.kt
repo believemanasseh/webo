201da783c29178854be26e59aeac0fba7ee572e7
Manasseh <believemanasseh@gmail.com>
2023-03-03 17:17:15 +0100

    modify profile and user models

---
+++ src/main/kotlin/xyz/webo/models/Profiles.kt
@@ -0,0 +1,18 @@
+package xyz.webo.models
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.statements.api.ExposedBlob
+import java.time.LocalDateTime
+
+object Profile : IntIdTable() {
+    val name: Column<String> = varchar("name", 50)
+    val bio: Column<String?> = varchar("bio", 50).nullable()
+    val location: Column<String?> = varchar("location", 50).nullable()
+    val website: Column<String?> = varchar("website", 100).nullable()
+    val displayPicture: Column<ExposedBlob?> = blob("display picture").nullable()
+    val bannerPicture: Column<ExposedBlob?> = blob("banner picture").nullable()
+    val dateOfBirth: Column<LocalDateTime?> = datetime("date of birth").nullable()
+    val userId = integer("user id").references(User.id)
+}
--- src/main/kotlin/xyz/webo/models/Users.kt
+++ src/main/kotlin/xyz/webo/models/Users.kt
@@ -1,16 +1,16 @@
 package xyz.webo.models
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.Table
-
 import java.time.LocalDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
 
-object Users : Table() {
-    private val id: Column<Int> = integer("id").autoIncrement()
-    val name: Column<String> = varchar("name", 50)
-    val email: Column<String> = varchar("email", 50)
-//    val dateCreated: Column<LocalDateTime> = datetime(name="date created")
-//    val dateModified: Column<LocalDateTime> = datetime(name="date modified")
-
-    override val primaryKey = PrimaryKey(id)
+object User : IntIdTable() {
+    val handle: Column<String> = varchar("handle", 50).uniqueIndex()
+    val email: Column<String> = varchar("email", 50).uniqueIndex()
+    val password: Column<String> = varchar("password", 50)
+    val dateCreated: Column<LocalDateTime> = datetime("date created")
+    val dateModified: Column<LocalDateTime> = datetime("date modified")
+    val following = reference("following", User).nullable()
+    val followers = reference("followers", User).nullable()
 }
