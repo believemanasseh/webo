cfac53d308f453ff86a986132f7c375ad5546511
Manasseh <believemanasseh@gmail.com>
2023-03-04 21:03:06 +0100

    add admin routes, integrate mustache templating engine

--- src/main/kotlin/xyz/webo/routes/AdminRoutes.kt
+++ src/main/kotlin/xyz/webo/routes/AdminRoutes.kt
@@ -1,2 +1,16 @@
 package xyz.webo.routes
 
+import io.ktor.server.application.*
+import io.ktor.server.mustache.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.mustache.MustacheContent
+
+
+fun Route.adminRouting() {
+    route("/admin") {
+        get {
+            call.respond(MustacheContent("src/main/resources/static/index.hbs", mapOf("model" to mapOf("name" to "Users"))))
+        }
+    }
+}
