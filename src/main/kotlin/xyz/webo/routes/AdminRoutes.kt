1506bd1b4e35eb1bf126b199ec7644054067f703
Manasseh <believemanasseh@gmail.com>
2023-03-14 13:51:58 +0100

    add profiles route, create hbs template

--- src/main/kotlin/xyz/webo/routes/AdminRoutes.kt
+++ src/main/kotlin/xyz/webo/routes/AdminRoutes.kt
@@ -1,16 +1,44 @@
 package xyz.webo.routes
 
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import io.ktor.server.application.*
 import io.ktor.server.mustache.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.mustache.MustacheContent
+import xyz.webo.models.*
 
 
 fun Route.adminRouting() {
     route("/admin") {
         get {
-            call.respond(MustacheContent("src/main/resources/static/index.hbs", mapOf("model" to mapOf("name" to "Users"))))
+            val users = transaction {
+                Users.selectAll()
+            }
+            println(users)
+            call.respond(
+                MustacheContent(
+                    "src/main/resources/static/index.hbs",
+                    mapOf("model" to mapOf("name" to mapOf("users" to "User", "profiles" to "Profile")))
+                )
+            )
+        }
+        get("/users") {
+            call.respond(
+                MustacheContent(
+                    "src/main/resources/static/users.hbs",
+                    mapOf("model" to mapOf("name" to mapOf("users" to "User")))
+                )
+            )
+        }
+        get("/profiles") {
+            call.respond(
+                MustacheContent(
+                    "src/main/resources/static/profiles.hbs",
+                    mapOf("model" to "profiles")
+                )
+            )
         }
     }
 }
