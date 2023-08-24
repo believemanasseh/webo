b3d70d80716b77f911f36630afc0759d325e0061
Manasseh <believemanasseh@gmail.com>
2023-08-24 23:04:58 +0100

    created new application configuration class

--- src/main/kotlin/xyz/webo/Config.kt
+++ src/main/kotlin/xyz/webo/Config.kt
@@ -1,4 +1,15 @@
 package xyz.webo
 
+import io.github.cdimascio.dotenv.dotenv
+
+val dotenv = dotenv()
+
 object Config {
+    val DB_URL: String = dotenv.get("DB_URL")
+    val DB_DRIVER: String = dotenv.get("DB_DRIVER")
+    val DB_USER: String = dotenv.get("DB_USER")
+    val DB_PASSWORD: String = dotenv.get("DB_PASSWORD")
+    val ADMIN_USER_EMAIL: String = dotenv.get("ADMIN_USER_EMAIL")
+    val ADMIN_USER_HANDLE: String = dotenv.get("ADMIN_USER_HANDLE")
+    val ADMIN_USER_PASSWORD: String = dotenv.get("ADMIN_USER_PASSWORD")
 }
