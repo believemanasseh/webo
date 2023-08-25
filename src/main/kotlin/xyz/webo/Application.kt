f4fef8f7d83cd66beefb1f091376fc568cf358a7
Manasseh <believemanasseh@gmail.com>
2023-08-25 11:17:25 +0100

    configure logger, setup plugin

--- src/main/kotlin/xyz/webo/Application.kt
+++ src/main/kotlin/xyz/webo/Application.kt
@@ -1,7 +1,5 @@
 package xyz.webo
 
-import ch.qos.logback.classic.LoggerContext
-import ch.qos.logback.classic.joran.JoranConfigurator
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -9,11 +7,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.mustache.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
-import org.slf4j.LoggerFactory
-import xyz.webo.plugins.configureDatabase
-import xyz.webo.plugins.configureRouting
-import xyz.webo.plugins.configureSecurity
-import xyz.webo.plugins.configureSerialization
+import xyz.webo.plugins.*
 
 fun main() {
     embeddedServer(
@@ -25,15 +19,9 @@ fun main() {
 }
 
 fun Application.module() {
-    // Configure logger
-    val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
-    loggerContext.reset()
-    val configurator = JoranConfigurator()
-    configurator.context = loggerContext
-    configurator.doConfigure(ClassLoader.getSystemResourceAsStream("logback.xml"))
-
     install(Authentication)
     install(Mustache)
+    configureLogger()
     configureSerialization()
     install(CORS) {
         anyHost()
--- src/main/kotlin/xyz/webo/utils/common.kt
+++ src/main/kotlin/xyz/webo/utils/common.kt
@@ -1,1 +1,8 @@
 package xyz.webo.utils
+
+import io.ktor.server.application.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+// Retrieve logger instance
+fun getLogger(): Logger = LoggerFactory.getLogger(Application::class.java)
