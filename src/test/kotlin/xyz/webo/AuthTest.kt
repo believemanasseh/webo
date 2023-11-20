2b9569c08c408fca2798afa1a190da86b4b332f6
Manasseh <believemanasseh@gmail.com>
2023-11-20 04:54:47 +0100

    test: refactor test setup function

--- src/test/kotlin/xyz/webo/AuthTest.kt
+++ src/test/kotlin/xyz/webo/AuthTest.kt
@@ -2,10 +2,15 @@ package xyz.webo
 
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.testing.*
 import kotlinx.serialization.json.Json
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import xyz.webo.models.Tokens
 import xyz.webo.plugins.configureDatabase
 import xyz.webo.plugins.configureRouting
 import xyz.webo.plugins.configureSerialization
@@ -18,6 +23,25 @@ class AuthTest {
     @BeforeTest
     fun setUp() {
         embeddedServer(Netty, port = Config.TEST_SERVER_PORT.toInt()) {
+            install(Authentication) {
+                bearer {
+                    realm = "webo"
+                    authenticate { tokenCredential ->
+                        val token = transaction {
+                            Tokens.select { Tokens.value eq tokenCredential.token }.firstOrNull()
+                        }
+                        if (token != null) {
+                            if (tokenCredential.token == token[Tokens.value].toString()) {
+                                UserIdPrincipal(token[Tokens.value].toString())
+                            } else {
+                                null
+                            }
+                        } else {
+                            null
+                        }
+                    }
+                }
+            }
             configureSerialization()
             configureDatabase(testing = true)
             configureRouting()
@@ -26,7 +50,7 @@ class AuthTest {
 
     @Test
     fun testLogin() = testApplication {
-        val res = client.post("/v1/login") {
+        val res = client.post("http://localhost:8082/v1/login") {
             header(HttpHeaders.ContentType, ContentType.Application.Json)
             setBody(Json.encodeToString(UserSerializer.serializer(), UserSerializer("test@email.com", "password")))
         }
