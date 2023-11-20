dbedf61fd8db6e66438a81d5b268a27450be28d6
Manasseh <believemanasseh@gmail.com>
2023-11-20 03:19:21 +0100

    refactor: unauthenticate users endpoint

--- src/main/kotlin/xyz/webo/routes/UserRoutes.kt
+++ src/main/kotlin/xyz/webo/routes/UserRoutes.kt
@@ -2,7 +2,6 @@ package xyz.webo.routes
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -19,88 +18,87 @@ import xyz.webo.serializers.UserSerializer
 
 fun Route.userRouting() {
     route("/v1/users") {
-        authenticate {
-            get {
-                try {
-                    val users = transaction {
-                        val res = Users.selectAll()
-                        val token = call.principal<UserIdPrincipal>()
-                        if (token != null) {
-                            val tokenObj = Tokens.select { Tokens.value eq token.name }.first()
-                            res.map {
-                                UserSerializer(
-                                    id = it[Users.id],
-                                    email = it[Users.email],
-                                    handle = it[Users.handle],
-                                    password = it[Users.password],
-                                    token = tokenObj[Tokens.value],
-                                    dateCreated = it[Users.dateCreated].toString(),
-                                    dateModified = it[Users.dateModified].toString()
-                                )
-                            }
-                        } else {
-                            listOf()
-                        }
-                    }
-                    call.respond(
-                        status = HttpStatusCode.Accepted,
-                        UserResponse(
-                            status = "success",
-                            message = "User login successful",
-                            data = UserData.UserList(users)
+        get {
+            try {
+                val users = transaction {
+                    val res = Users.selectAll()
+                    res.map {
+                        val token = Tokens.select { Tokens.user eq it[Users.id] }.first()
+                        UserSerializer(
+                            id = it[Users.id],
+                            email = it[Users.email],
+                            handle = it[Users.handle],
+                            token = token[Tokens.value],
+                            password = it[Users.password],
+                            dateCreated = it[Users.dateCreated].toString(),
+                            dateModified = it[Users.dateModified].toString()
                         )
-                    )
-                } catch (e: Exception) {
-                    println(e)
-                    call.respond(
-                        status = HttpStatusCode.BadRequest,
-                        mapOf("status" to "error", "message" to "Invalid request")
-                    )
+                    }
                 }
+                call.respond(
+                    status = HttpStatusCode.Accepted,
+                    UserResponse(
+                        status = "success",
+                        message = "User login successful",
+                        data = UserData.UserList(users)
+                    )
+                )
+            } catch (e: Exception) {
+                println(e)
+                call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    mapOf("status" to "error", "message" to "Invalid request")
+                )
             }
         }
-        authenticate {
-            get("/v1/users/{id}") {
-                try {
-                    val id: Int? = call.parameters["id"]?.toInt()
-                    if (id !is Int) {
-                        call.respond(mapOf("status" to "error", "message" to "Invalid path param"))
+    }
+    route("/v1/users/{id}") {
+        get {
+            try {
+                val id: Int? = call.parameters["id"]?.toInt()
+                if (id !is Int) {
+                    call.respond(mapOf("status" to "error", "message" to "Invalid path param"))
+                }
+                val user = async {
+                    transaction {
+                        Users.select { Users.id eq id }.firstOrNull()
                     }
-                    val user = async {
-                        transaction {
-                            Users.select { Users.id eq id }.firstOrNull()
-                        }
+                }
+                val token = async {
+                    transaction {
+                        Tokens.select { Tokens.user eq id }.firstOrNull()
                     }
-                    if (user.await() == null) {
-                        call.respond(
-                            status = HttpStatusCode.NotFound,
-                            mapOf("status" to "error", "message" to "User does not exist")
-                        )
-                    }
+                }
+                if (user.await() == null) {
                     call.respond(
-                        status = HttpStatusCode.Accepted,
-                        UserResponse(
-                            status = "success",
-                            message = "User retrieved successfully",
-                            data = UserData.SingleUser(
-                                UserSerializer(
-                                    id = user.await()!![Users.id],
-                                    email = user.await()!![Users.email],
-                                    handle = user.await()!![Users.handle],
-                                    dateCreated = user.await()!![Users.dateCreated].toString(),
-                                    dateModified = user.await()!![Users.dateModified].toString()
-                                )
+                        status = HttpStatusCode.NotFound,
+                        mapOf("status" to "error", "message" to "User does not exist")
+                    )
+                }
+                call.respond(
+                    status = HttpStatusCode.Accepted,
+                    UserResponse(
+                        status = "success",
+                        message = "User retrieved successfully",
+                        data = UserData.SingleUser(
+                            UserSerializer(
+                                id = user.await()!![Users.id],
+                                email = user.await()!![Users.email],
+                                handle = user.await()!![Users.handle],
+                                token = token.await()!![Tokens.value],
+                                password = user.await()!![Users.password],
+                                dateCreated = user.await()!![Users.dateCreated].toString(),
+                                dateModified = user.await()!![Users.dateModified].toString()
                             )
                         )
                     )
-                } catch (e: BadRequestException) {
-                    call.respond(
-                        status = HttpStatusCode.BadRequest,
-                        mapOf("status" to "error", "message" to "Invalid request")
-                    )
-                }
+                )
+            } catch (e: BadRequestException) {
+                call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    mapOf("status" to "error", "message" to "Invalid request")
+                )
             }
         }
-
     }
 }
