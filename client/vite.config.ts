3bdfb4b99b1fac8008aa97ca7093e2e4423487c0
Manasseh <believemanasseh@gmail.com>
2024-06-19 02:03:06 +0100

    chore: modify vike config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,16 @@
 import { UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import vike from "vike/plugin";
+import vercel from "vite-plugin-vercel";
 import wyw from "@wyw-in-js/vite";
 
 const config: UserConfig = {
   plugins: [
+    vercel(),
     react(),
-    vike({ prerender: true }),
+    vike({
+      prerender: { partial: true },
+    }),
     wyw({
       include: ["**/*.{ts,tsx}"],
       babelOptions: {
