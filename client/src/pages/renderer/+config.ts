6268d3e6e85e75c72033321ade4e4b57771bfc68
Manasseh <believemanasseh@gmail.com>
2024-06-20 13:43:25 +0100

    feat: add page title and favicon

--- client/src/pages/renderer/+config.ts
+++ client/src/pages/renderer/+config.ts
@@ -5,6 +5,8 @@ import vikeReact from "vike-react/config";
 export default {
   clientRouting: true,
   prerender: false,
+  title: "Webo - Right to Ownership",
+  favicon: "/webo.png",
   ssr: false,
   extends: [vercelConfig, vikeReact],
   onRenderClient: "import:vike-react/renderer/onRenderClient:onRenderClient",
